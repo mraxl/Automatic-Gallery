d380a9a72ba8b49c5590215023c0bf6e045fb316
mraxl <mraxl89@gmail.com>
2015-04-14 16:23:28 -0300

    Solicitar nombre carpetas y tipo de archivos
    Se solicita tipo de archivos y nombre de la carpeta

--- galeria.php
+++ galeria.php
@@ -17,22 +17,24 @@
 </script>
 <?php
 require("functions.php");
-$files=get_files("images",array('jpeg','jpg','png'));
+$folder="images";
+$files=get_files($folder,array('jpeg','jpg','png'));
+
 $i=0;
 foreach($files as $file){
 	if($i==0){
 		echo "<figure id='imagen-principal'>
-			<img src='images/$file'/>
+			<img src='$folder/$file'/>
 			</figure>";
 		echo "<div id='contiene-imagenes'>
 			  <div id='slide-images'>";
 		echo "<figure class='image-tumb first'>
-			  	<img src='images/$file' />
+			  	<img src='$folder/$file' />
 			  </figure>";
 			  $i++;
 		}else{
 			echo "<figure class='image-tumb'>
-			  		<img src='images/$file' />
+			  		<img src='$folder/$file' />
 			  	</figure>";
 		}
 }
