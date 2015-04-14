383c5c2f123c5083051ced65a1dadaa4b5d2a735
mraxl <mraxl89@gmail.com>
2015-04-14 03:10:38 -0300

    Start
    Gallery Automatic from PHP and Jquery + CSS
    
    -----
    14/04/2015
    
    Only CSS and HTML
    ----------------

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function get_files($images_dir,$exts = array('jpg')) {
+	$files = array();
+	if($handle = opendir($images_dir)) {
+		while(false !== ($file = readdir($handle))) {
+			$extension = strtolower(get_file_extension($file));
+			if($extension && in_array($extension,$exts)) {
+				$files[] = $file;
+			}
+		}
+		closedir($handle);
+	}
+	return $files;
+}
+function get_file_extension($file_name) {
+	return substr(strrchr($file_name,'.'),1);
+}
+?>
